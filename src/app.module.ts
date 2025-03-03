fc5de633cf05f412a697b0a3cc9eaf36618dec9c
renan.beraldi <renanberaldi@outlook.com>
2025-03-02 21:05:36 -0300

    Feat: Implemented imports in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UsersModule } from "./users/modules/UsersModule";
+import { AuthModule } from "./auth/modules/AuthModule";
+import { TransactionsModule } from "./transactions/modules/TransactionsModule";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: "postgres",
+            host: "localhost",
+            port: 5432,
+            username: "postgres",
+            password: "Regis22041980!",
+            database: "omni",
+            autoLoadEntities: true,
+        }),
+        UsersModule,
+        AuthModule,
+        TransactionsModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
